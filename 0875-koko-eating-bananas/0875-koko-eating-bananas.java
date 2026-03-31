87698661b6a52d786f7ce8ca5c9b72557c334185
Piyush Gola <153226079+piysgo@users.noreply.github.com>
2026-03-31 16:47:01 +0530

    Time: 9 ms (61.85%), Space: 47.7 MB (90.41%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int maxVal = Arrays.stream(piles).max().getAsInt();
+        int low = 1, high = maxVal;
+        int ans =0;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            long result = Totalhrs(piles, mid);
+            if(result<=h){
+                ans = mid;
+                high = mid-1;
+            }else{
+                low = mid+1;
+            }
+        }
+        return ans;
+        
+    }
+    public static long Totalhrs(int[] piles, int mid){
+        long hrs=0;
+        for(int i:piles){
+        hrs += (i + mid - 1) / mid;        }
+        return hrs;
+    }
+}
