f8cc95bb113c7f79a34126a05e42845ec3e660fb
Piyush Gola <153226079+piysgo@users.noreply.github.com>
2026-05-22 23:56:43 +0530

    Time: 16 ms (44.03%), Space: 43.9 MB (41.15%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,22 +1,30 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        HashMap<Character, Integer> charIndexS = new HashMap<>();
-        HashMap<Character, Integer> charIndexT = new HashMap<>();
+        int n = s.length();
+        HashMap <Character, Character> hash1 = new HashMap<>();
+        HashMap <Character, Character> hash2 = new HashMap<>();
+        
+        for(int i=0;i<n;i++){
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            if(hash1.containsKey(c1)){
+                if(hash1.get(c1)!=c2){
+                    return false;
+                }
+                }else{
+                    hash1.put(c1, c2);
+                }
 
-        for (int i = 0; i < s.length(); i++) {
-            if (!charIndexS.containsKey(s.charAt(i))) {
-                charIndexS.put(s.charAt(i), i);
+            if(hash2.containsKey(c2)){
+                if(hash2.get(c2) != c1){
+                    return false;
+                }
+            }else{
+                hash2.put(c2,c1);
             }
-
-            if (!charIndexT.containsKey(t.charAt(i))) {
-                charIndexT.put(t.charAt(i), i);
             }
-
-            if (!charIndexS.get(s.charAt(i)).equals(charIndexT.get(t.charAt(i)))) {
-                return false;
-            }
+            return true;
         }
-
-        return true;        
+        
+        
     }
-}
