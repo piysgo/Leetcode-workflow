6848cd87e1cb12b28a70c0a65c42678a6452c146
Piyush Gola <153226079+piysgo@users.noreply.github.com>
2026-05-24 01:07:04 +0530

    Time: 0 ms (100%), Space: 42.9 MB (40.83%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.java
+++ 0796-rotate-string/0796-rotate-string.java
@@ -3,13 +3,7 @@ class Solution {
         if (s.length() != goal.length()) {
             return false;
         }
-        int n = s.length();
-        for (int i = 0; i < n; i++) {
-            s = s.substring(1) + s.charAt(0);
-            if (s.equals(goal)) {
-                return true;
-            }
-        }
-        return false;
+        String ans = s + s;
+        return ans.contains(goal);
     }
 }
